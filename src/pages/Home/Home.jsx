e87facc381a764071a8da634ef3b3d812c6856a4
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2024-09-21 22:46:26 +0500

    add hero Section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,30 @@
-import Navbar from "../../components/Navbar/Navbar"
-import "./Home.css"
+import Navbar from "../../components/Navbar/Navbar";
+import "./Home.css";
+import hero_banner from "../../assets/hero_banner.jpg";
+import hero_title from "../../assets/hero_title.png";
+import play_icon from "../../assets/play_icon.png";
+import info_icon from "../../assets/info_icon.png";
 
 export const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <div className="hero">
+        <img src={hero_banner} alt="" className="banner-img" />
+        <div className="hero-caption">
+          <img src={hero_title} alt="" className="caption-img" />
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi,
+            quia a hic nam repellendus cumque quos dolorem dolorum beatae iste
+            consequuntur recusandae.
+          </p>
+          <div className="hero-btn">
+            <button className="btn"><img src={play_icon} alt="" />Play</button>
+            <button className="btn dark-btn"><img src={info_icon} alt="" />More info</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
-export default Home
+  );
+};
+export default Home;
