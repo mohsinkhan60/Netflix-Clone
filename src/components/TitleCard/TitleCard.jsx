914cc41be15b683e81a822a17ea143706e79b779
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2024-09-22 02:13:33 +0500

    Fix deployment Error

--- src/components/TitleCard/TitleCard.jsx
+++ src/components/TitleCard/TitleCard.jsx
@@ -1,35 +1,32 @@
-import "./TitleCard.css"
-import cards_data from "../../assets/cards/cards_data"
-import { useRef } from "react"
-import { useEffect } from "react"
+import "./TitleCard.css";
+import cards_data from "../../assets/cards/cards_data.json";
+import { useRef } from "react";
+import { useEffect } from "react";
 
 export const TitleCard = () => {
-  const cardsRef = useRef(null)
+  const cardsRef = useRef(null);
 
   const handleWheel = (e) => {
-    e.preventDefault()
-    cardsRef.current.scrollLeft += e.deltaY
-  }
+    e.preventDefault();
+    cardsRef.current.scrollLeft += e.deltaY;
+  };
   useEffect(() => {
-    cardsRef.current.addEventListener("wheel", handleWheel)
-  },[])
+    cardsRef.current.addEventListener("wheel", handleWheel);
+  }, []);
   return (
     <div className="titleCards">
       <h2>Popuar on Netflix</h2>
       <div className="card-list" ref={cardsRef}>
-        {
-          cards_data.map((card, index) => {
-            return (
-              <div className="card" key={index}>
-                <img src={card.image} alt="" />
-                <p>{card.name}</p>
-              </div>
-            )
-        })
-      }
-        
+        {cards_data.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} alt="" />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
-  )
-}
-export default TitleCard
+  );
+};
+export default TitleCard;
