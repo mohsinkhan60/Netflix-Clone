872bf45a1f08cf6a5a74292e1ccc43b4fb22bc23
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2024-09-22 19:43:58 +0500

    add Sign up Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import Home from "./pages/Home/Home"
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
 
 export const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
-  )
-}
-export default App
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+};
+
+export default App;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,31 @@
 import "./Login.css"
+import logo from "../../assets/logo.png"
 
 export const Login = () => {
   return (
-    <div className="login">Login</div>
+    <div className="login">
+      <img src={logo} className="login-logo" alt="" />
+      <div className="login-form">
+        <h1>Sign up</h1>
+        <form>
+          <input type="text" placeholder="Your name..." />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>Sign up</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need Help</p>
+          </div>
+        </form>
+        <div className="form-switch">
+          <p>New To Netflix ? <span>Sign up Now</span></p>
+          <p>Already have account?<span>Sign In Now</span></p>
+        </div>
+      </div>
+    </div>
   )
 }
 export default Login
